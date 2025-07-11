85df578336d18516cde52ffb7e635f031a6541fa
Pragyan Sen <146320930+Pragyan2027@users.noreply.github.com>
2025-07-11 14:11:54 +0530

    Time: 3 ms (12.03%) | Memory: 48.5 MB (8.32%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,13 +1,38 @@
 class Solution {
-    public int maxProfit(int[] prices) {
+    private int[][] dp;
+
+    private int solve(int index, boolean buy, int[] prices, int n) {
+        if (index == n) {
+            return 0;
+        }
+        int buyIndex = buy ? 1 : 0;
+        if (dp[index][buyIndex] != -1) {
+            return dp[index][buyIndex];
+        }
+
         int profit = 0;
-        
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                profit += prices[i] - prices[i - 1];
+        if (buy) {
+            int pick = -prices[index] + solve(index + 1, false, prices, n);
+            int notPick = solve(index + 1, true, prices, n);
+            profit = Math.max(pick, notPick);
+        } else {
+            int pick = prices[index] + solve(index + 1, true, prices, n);
+            int notPick = solve(index + 1, false, prices, n);
+            profit = Math.max(pick, notPick);
+        }
+
+        dp[index][buyIndex] = profit;
+        return profit;
+    }
+
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        dp = new int[n][2]; // 2 states: buy or not buy
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
+                dp[i][j] = -1;
             }
         }
-        
-        return profit;        
+        return solve(0, true, prices, n);
     }
 }
